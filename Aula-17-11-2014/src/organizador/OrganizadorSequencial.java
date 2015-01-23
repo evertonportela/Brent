c727767aef2e08f5c5c28b124d92e49ba5e4d70e
braynner <max.braynner@gmail.com>
2015-01-23 20:31:02 -0300

    Correções

--- Aula-17-11-2014/src/organizador/OrganizadorSequencial.java
+++ Aula-17-11-2014/src/organizador/OrganizadorSequencial.java
@@ -1,46 +1,65 @@
 package organizador;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.List;
 
 import aluno.Aluno;
 
+public class OrganizadorSequencial implements IFileOrganizer {
 
-public class OrganizadorSequencial implements IFileOrganizer{
-
+	/**
+	 * Canal de escrita e leitura no arquivo
+	 */
 	private FileChannel canal;
+
+	/**
+	 * Valor estatico usado para indicar que a consulta não retornou registro
+	 */
 	private static final long INEXISTENTE = -1;
-	
-	public OrganizadorSequencial(String arqName) throws FileNotFoundException{
+
+	/**
+	 * 
+	 * @param arqName
+	 *            Nome do arquivo a ser utilizado como fonte de dados
+	 * @throws FileNotFoundException
+	 */
+	public OrganizadorSequencial(String arqName) throws FileNotFoundException {
+		// Cria ou acessa o arquivo no diretório passado
 		File file = new File(arqName);
+
+		// Representa um arquivo usado para armazenar os dados que podem ser
+		// acessados de maneira não-seqüencial.
 		RandomAccessFile raf = new RandomAccessFile(file, "rw");
+
 		this.canal = raf.getChannel();
-		//raf.close();
+		// raf.close();
 	}
-	
+
 	@Override
 	public boolean addReg(Aluno pAluno) {
-		try{
+		try {
 			long position = this.getPositionMaior(pAluno.getMatricula());
-			
+
 			// Se não encontrar maior ecreve no fim
-			if(position == this.INEXISTENTE){
-				canal.write(pAluno.getBuffer(), canal.size());				
-			}else{
+			if (position == this.INEXISTENTE) {
+				canal.write(pAluno.getBuffer(), canal.size());
+			} else {
 				// Move todos os maiores para frente
-				for(long i=canal.size(); i>=position; i-=Aluno.LENGTH){
-					ByteBuffer buff = alocarAluno(i - Aluno.LENGTH, Aluno.LENGTH);
+				for (long i = canal.size(); i >= position; i -= Aluno.LENGTH) {
+					ByteBuffer buff = alocarAluno(i - Aluno.LENGTH,
+							Aluno.LENGTH);
 					canal.write(buff, i);
+					buff.flip();
 				}
 				// Escreve na posição correta
 				canal.write(pAluno.getBuffer(), position);
 			}
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
@@ -48,101 +67,125 @@ public class OrganizadorSequencial implements IFileOrganizer{
 	}
 
 	@Override
-	public Aluno getReg(int pMatricula){
-		try{
+	public Aluno getReg(int pMatricula) {
+		try {
 			long position = this.getPosition(pMatricula);
-			if(position==this.INEXISTENTE){
+			if (position == this.INEXISTENTE) {
 				return null;
-			}else{
+			} else {
 				return new Aluno(this.alocarAluno(position, Aluno.LENGTH));
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
 
 	@Override
-	public Aluno delReg(int pMatricula){
-		try{
+	public Aluno delReg(int pMatricula) {
+		try {
 			Aluno alunoDeletado = null;
 			ByteBuffer buff = null;
-			
+
 			long position = this.getPosition(pMatricula);
-			
-			if(position==this.INEXISTENTE){
+
+			if (position == this.INEXISTENTE) {
 				return alunoDeletado;
-			}else{
+			} else {
 				buff = alocarAluno(position, Aluno.LENGTH);
 				alunoDeletado = new Aluno(buff);
 
 				// Move todos os maiores para frente caso não seja o ultimo
-				if(position + Aluno.LENGTH != (this.canal.size()-1)){				
-					for(long i=position; i<canal.size(); i+=Aluno.LENGTH){
-						buff = alocarAluno(i, Aluno.LENGTH);
+				if (position + Aluno.LENGTH != (this.canal.size() - 1)) {
+					for (long i = position; i < canal.size() - Aluno.LENGTH; i += Aluno.LENGTH) {
+						long can = canal.size();
+						
+						//Pega o próximo aluno
+						buff = alocarAluno(i + Aluno.LENGTH, Aluno.LENGTH);
+						
+						//Sobrescreve o aluno no registro anterior
 						canal.write(buff, i);
+						buff.flip();
 					}
 					// Recebe a posição do truncate
-					position = this.canal.size()-Aluno.LENGTH;
+					position = this.canal.size() - Aluno.LENGTH;
 				}
-				
+
 				this.canal.truncate(position);
 			}
 
 			return alunoDeletado;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	/**
+	 * Obtem a posição de um Aluno no arquivo
 	 * 
 	 * @param matricula
-	 * @return
-	 * @throws IOException 
+	 *            Matricula do aluno pesquisado
+	 * @return pos posição do aluno no arquivo. Caso não seja encontrado aluno,
+	 *         retorna o valor estatico de INEXISTENTE (-1)
+	 * @throws IOException
 	 */
-	private long getPosition(int pMatricula) throws IOException{
+	private long getPosition(int pMatricula) throws IOException {
 		long size = this.canal.size();
-		
+
 		// Coloca o canal na posição 0
-		for(long pos=0; pos<size; pos+=Aluno.LENGTH){
+		for (long pos = 0; pos < size; pos += Aluno.LENGTH) {
 			ByteBuffer buff = alocarAluno(pos, 4);
-			
-			if(pMatricula==buff.getInt()){
+			int mat = buff.getInt();
+			if (pMatricula == mat) {
 				return pos;
-			}else if(buff.getInt() > pMatricula){
+			} else if (mat > pMatricula) {
 				break;
 			}
+			buff.flip();
 		}
-		
+
 		return this.INEXISTENTE;
 	}
 
-	private ByteBuffer alocarAluno(long pos, int pBuffTamanho) throws IOException {
+	/**
+	 * Aloca uma instância de Aluno ou parte dos atributos deste em um
+	 * ByteBuffer para recuperar os valores gravados
+	 * 
+	 * @param pos
+	 *            Posição apartir da qual ira obter os valores do aluno
+	 * @param pBuffTamanho
+	 *            Tamanho do buffer. Varia a depender do que pretende obter.
+	 *            Para matricula, apenas 4, matricula e nome 4 + 50...
+	 * @return buff ByteBuffer
+	 * @throws IOException
+	 */
+	private ByteBuffer alocarAluno(long pos, int pBuffTamanho)
+			throws IOException {
 		ByteBuffer buff = ByteBuffer.allocate(pBuffTamanho);
 		this.canal.read(buff, pos);
 		buff.flip();
 		return buff;
 	}
-	
-	private long getPositionMaior(int pMatricula) throws IOException{
+
+	/**
+	 * Dada uma matricula, retorna o 1 Aluno com matricula imediatamente maior do que a matricula passada por parametro
+	 * @param pMatricula Matricula do Aluno a ser comparada
+	 * @return Matricula do Aluno com matricula imediatamente maior do que a matricula passada por parametro
+	 * @throws IOException
+	 */
+	private long getPositionMaior(int pMatricula) throws IOException {
 		long size = this.canal.size();
 
 		// Coloca o canal na posição 0
-		for(long pos=0; pos<size; pos+=Aluno.LENGTH){
+		for (long pos = 0; pos < size; pos += Aluno.LENGTH) {
 			ByteBuffer buff = alocarAluno(pos, 4);
-			
-			if(buff.getInt() > pMatricula){
+
+			if (buff.getInt() > pMatricula) {
 				return pos;
 			}
+			buff.flip();
 		}
 
 		return this.INEXISTENTE;
 	}
-	
-	@Override
-	public List<Aluno> listar() {
-		
-		
-		return null;
-	}
+
 }
