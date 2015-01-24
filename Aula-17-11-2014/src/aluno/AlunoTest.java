037dda6743f4b2c7fa9515bf31f18a1b7bd69646
braynner <max.braynner@gmail.com>
2015-01-23 22:32:36 -0300

    Alterações para gerar a matricula do Aluno aleatoriamente (0 a 100) sem repetições.

--- Aula-17-11-2014/src/aluno/AlunoTest.java
+++ Aula-17-11-2014/src/aluno/AlunoTest.java
@@ -1,36 +1,59 @@
 package aluno;
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
 
 import organizador.IFileOrganizer;
 import organizador.OrganizadorSequencial;
 
 
 public class AlunoTest {
+	/**
+	 * - Gera um número aleatório de 0 a 100 
+	 * que não esteja nas matriculas que já foram geradas
+	 * 
+	 * @param matriculasExistentes
+	 * @return
+	 */
+	public int getMatricula(ArrayList<Integer> matriculasExistentes){
+		int matricula = (int) (1 + (Math.random() * 100));
+		if(matriculasExistentes.contains(matricula)){
+			return getMatricula(matriculasExistentes);
+		}else{
+			return matricula;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
+		AlunoTest alunoTest = new AlunoTest();
+		
+		// Array utilizado para guardar todas as matriculas
+		ArrayList<Integer> arrayMatriculas = new ArrayList<Integer>();
+		
 		IFileOrganizer organizador = new OrganizadorSequencial("alunos.db");		
-		int mat = 1;
-		while(mat<=20){
-			Aluno aluno = new Aluno(mat, "Joao"+mat, "Rua A 4151", (short)10,	"M", "joao@mail.com");
+		for(int i = 0; i<20; i++){
+			int matricula = alunoTest.getMatricula(arrayMatriculas);
+
+			// Adiciona a matricula para não haver repetições
+			arrayMatriculas.add(matricula);
+			
+			Aluno aluno = new Aluno(matricula, "Joao-"+matricula, "Rua A 4151", (short)10,	"M", "joao@mail.com");
 			organizador.addReg(aluno);
-			mat++;
 		}
 		
-		Aluno aluno = new Aluno(10, "Joao 10B", "Rua A 4151", (short)20,	"M", "joao@mail.com");
-		organizador.addReg(aluno);
-		Aluno aluno2 = new Aluno(12, "Joao 12B", "Rua A 4151", (short)20,	"M", "joao@mail.com");
-		organizador.addReg(aluno2);
-		
+		// Busca por esta matricula, depois a exclui e confirma se foi excluida
+		int excluirmatricula = arrayMatriculas.get(0);
 		
 		System.out.println("Busca inicial...");
-		Aluno alunoManipulado = organizador.getReg(15);
+		Aluno alunoManipulado = organizador.getReg(excluirmatricula);
 		System.out.println(alunoManipulado);
 		
 		System.out.println(" \nDeletando... ");
-		System.out.println(organizador.delReg(15));
+		System.out.println(organizador.delReg(excluirmatricula));
 		
 		System.out.println(" \nBuscando...");
-		alunoManipulado = organizador.getReg(15);
+		alunoManipulado = organizador.getReg(excluirmatricula);
 		System.out.println(alunoManipulado);
 	}
+	
 }
