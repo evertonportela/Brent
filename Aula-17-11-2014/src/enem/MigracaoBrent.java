c8424882210504253a121f4f53c4c56ab7aa43a4
braynner <max.braynner@gmail.com>
2015-01-28 18:16:50 -0300

    Adicionado ao laço uma posição de parada.

--- Aula-17-11-2014/src/enem/MigracaoBrent.java
+++ Aula-17-11-2014/src/enem/MigracaoBrent.java
@@ -32,6 +32,11 @@ public class MigracaoBrent{
 
 				// Inserir no destino
 				org.addReg(aluno);
+
+				// posição de parada
+				if (pos>(10*Aluno.LENGTH)) {
+					break;
+				}
 			}
 			channelOrigem.close();
 			fileOrigem.close();
