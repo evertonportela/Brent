8d4416cb4a0864f99e0e6b451deed5e00fca12c4
braynner <max.braynner@gmail.com>
2015-01-31 12:08:00 -0300

    Lista todos os alunos no fim das inserções

--- Aula-17-11-2014/src/br/com/apresentacao/ApresentacaoBrent.java
+++ Aula-17-11-2014/src/br/com/apresentacao/ApresentacaoBrent.java
@@ -1,5 +1,10 @@
 package br.com.apresentacao;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 import aluno.Aluno;
 import organizador.OrganizadorBrent;
 
@@ -15,12 +20,30 @@ public class ApresentacaoBrent {
 					(short) 18, "M", "aluno@ufs.br"));
 
 			organizadorBrent.addReg(new Aluno(18, "Aluno", "Rua 10",
-					(short) 18, "M", "aluno@ufs.br"));*/
-			
-			organizadorBrent.addReg(new Aluno(29, "Aluno", "Rua 10",
 					(short) 18, "M", "aluno@ufs.br"));
 			
+			organizadorBrent.addReg(new Aluno(29, "Aluno "+29, "Rua",
+					(short) 18, "M", "mail"));*/
 			
+			
+			// varre o arquivo e mostra as posições
+			File fOrigem = new File(GerarArquivo.NM_ARQUIVO_APRESENTACAO);
+			RandomAccessFile randomAccessFile = new RandomAccessFile(fOrigem, "r");
+			FileChannel canal = randomAccessFile.getChannel();
+			
+			for (long pos=0; pos < canal.size(); pos +=Aluno.LENGTH)  {
+				// Ler da origem
+				ByteBuffer buff = ByteBuffer.allocate(Aluno.LENGTH);
+				canal.read(buff, pos);
+
+				buff.flip();
+				Aluno aluno = new Aluno(buff);
+
+				System.out.println("Posição: "+ (pos/Aluno.LENGTH)+ " - Matricula: "+ aluno.getMatricula());
+			}
+			
+			canal.close();
+			randomAccessFile.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
