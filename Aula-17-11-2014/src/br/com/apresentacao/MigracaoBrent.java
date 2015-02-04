e84407942bbd83f4c435c176bf73041332246177
braynner <max.braynner@gmail.com>
2015-02-03 21:25:50 -0300

    Remoção do acompahamento

--- Aula-17-11-2014/src/br/com/apresentacao/MigracaoBrent.java
+++ Aula-17-11-2014/src/br/com/apresentacao/MigracaoBrent.java
@@ -12,10 +12,7 @@ import aluno.Aluno;
 
 public class MigracaoBrent{
 	
-	public static int contador = 0;
-
 	public static void main(String[] args){
-		Acompanhamento acompanhamento = new Acompanhamento();
 		try {
 			File fOrigem = new File("enem_aleat.db");
 			RandomAccessFile fileOrigem;
@@ -23,8 +20,6 @@ public class MigracaoBrent{
 			FileChannel channelOrigem = fileOrigem.getChannel();
 
 			IFileOrganizer org = new OrganizadorBrent("enem_brent.db");
-
-			acompanhamento.start();
 
 			// Ler cada aluno do arquivo de origem e inserir no de destino
 			for (int pos=0; pos < channelOrigem.size(); pos +=Aluno.LENGTH)  {
@@ -37,14 +32,11 @@ public class MigracaoBrent{
 
 				// Inserir no destino
 				org.addReg(aluno);
-				contador++;
 			}
 			channelOrigem.close();
 			fileOrigem.close();
-			acompanhamento.stop();
 		} catch (Exception e) {
 			e.printStackTrace();
-			acompanhamento.stop();
 		}
 	}
 }
